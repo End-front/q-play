d994c8a9d72887781f1676f032ec736574fa488f
End-front <62587226+End-front@users.noreply.github.com>
2021-06-03 21:37:46 +0500

    Add files via upload

--- js/checkbrowser.js
+++ js/checkbrowser.js
@@ -1,15 +1,1 @@
-"use strict";
-
-window.checkBrowser = {};
-window.checkBrowser.supportsTouch = !!('ontouchstart' in window || navigator.maxTouchPoints);
-window.checkBrowser.isIE = !!(window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1);
-
-if (window.checkBrowser.supportsTouch) {
-  document.documentElement.classList.add('touch-browser');
-} else {
-  document.documentElement.classList.add('courser-browser');
-}
-
-if (window.checkBrowser.isIE) {
-  document.documentElement.classList.add('ie-11');
-}
+"use strict";window.checkBrowser={},window.checkBrowser.supportsTouch=!!("ontouchstart"in window||navigator.maxTouchPoints),window.checkBrowser.isIE=!!(-1<window.navigator.userAgent.indexOf("MSIE ")||-1<window.navigator.userAgent.indexOf("Trident/")),window.checkBrowser.supportsTouch?document.documentElement.classList.add("touch-browser"):document.documentElement.classList.add("courser-browser"),window.checkBrowser.isIE&&document.documentElement.classList.add("ie-11");
